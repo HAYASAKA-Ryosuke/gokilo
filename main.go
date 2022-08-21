acc25b9b3999ca1fe069451a8dfaef853efcfe9e
HAYASAKA Ryosuke <hayasaka.ryosuke@gmail.com>
2022-08-22 00:13:04 +0900

    add statusbar add rendertext

--- main.go
+++ main.go
@@ -9,50 +9,71 @@ import (
 )
 
 type EditorRow struct {
-	text string
+	text       string // データとしてもっておく文字列
+	renderText string // 表示用の文字列
 }
 
 var (
-	windowX              = 0
-	windowY              = 0
-	currentX             = 0
-	currentY             = 0
+	DEBUG                = "debug"
+	currentColumn        = 0
+	currentRow           = 0
+	renderColumn         = 0
+	renderRow            = 0
 	autoCompletionEnable = false
 	editorBuf            = ""
-	editorRow            = []EditorRow{EditorRow{""}}
+	rowOffset            = 0
+	columnOffset         = 0
+	editorRows           = []EditorRow{EditorRow{"", ""}}
 	defStyle             = tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
+	windowSizeColumn     = 0
+	windowSizeRow        = 0
 )
 
+func drawContent(s tcell.Screen, column, row int, text string) {
+	for _, r := range []rune(text) {
+		s.SetContent(column, row, r, nil, defStyle)
+		column++
+	}
+
+}
+
+func drawStatusBar(s tcell.Screen) {
+	drawContent(s, 0, windowSizeRow-1, fmt.Sprintf("status %d, %d, %s", currentColumn, currentRow, DEBUG))
+}
+
 func editorDrawRows(s tcell.Screen) {
-	for y := 0; y < 24; y++ {
-		for _, r := range []rune("~") {
-			s.SetContent(0, y, r, nil, defStyle)
-		}
+	//for y := currentRow; y < windowSizeRow; y++ {
+	//	drawContent(s, 0, y, strconv.Itoa(y+1))
+	//}
+	drawContent(s, renderColumn, renderRow, editorRows[currentRow].renderText)
+	drawStatusBar(s)
+}
+
+func editorAppendRow(c string) {
+	editorRowLength := len(editorRows) - 1
+	editorRows[editorRowLength].text += c
+	updateRenderRow(editorRows[editorRowLength])
+}
+
+func updateRenderRow(row EditorRow) {
+	row.renderText = row.text
+}
+
+func editorScroll(c tcell.Screen) {
+	if currentRow < rowOffset {
+		rowOffset = currentRow
+	}
+	if currentRow >= rowOffset+windowSizeRow {
+		rowOffset = currentRow - windowSizeRow + 1
 	}
 }
 
-func drawText(s tcell.Screen, x1, y1, x2, y2 int, style tcell.Style, text string) {
-	row := y1
-	col := x1
-	windowWidth, _ := s.Size()
-	for _, r := range []rune(text) {
-		if windowWidth < x1 {
-			col++
-			windowY++
-		}
-		s.SetContent(col, row, r, nil, style)
-		col++
-		if col >= x2 {
-			row++
-			col = x1
-		}
-		if row > y2 {
-			break
-		}
-	}
+func getWindowSize(s tcell.Screen) (int, int) {
+	return s.Size()
 }
 
 func editorRefreshScreen(s tcell.Screen) {
+	editorScroll(s)
 	s.Clear()
 }
 
@@ -70,11 +91,32 @@ func editorProcessKeyPress(s tcell.Screen, ev *tcell.EventKey) {
 	} else if ev.Key() == tcell.KeyCtrlP {
 	} else if ev.Key() == tcell.KeyEnter {
 	} else if ev.Key() == tcell.KeyLeft {
+		if currentColumn != 0 {
+			currentColumn--
+		} else if currentColumn == 0 && currentRow != 0 {
+			currentColumn = 0
+			currentRow--
+		}
 	} else if ev.Key() == tcell.KeyRight {
+		if currentColumn != windowSizeColumn-1 {
+			currentColumn++
+		} else if currentColumn == windowSizeColumn-1 && currentRow != windowSizeRow-1 {
+			currentColumn = 0
+			currentRow++
+		}
 	} else if ev.Key() == tcell.KeyDown {
+		if currentRow != windowSizeRow-1 {
+			currentRow++
+		}
 	} else if ev.Key() == tcell.KeyUp {
+		if currentRow != 0 {
+			currentRow--
+		}
 	} else {
 	}
+}
+
+func initialize(s tcell.Screen) {
 
 }
 
@@ -100,9 +142,12 @@ func main() {
 	ox, oy := -1, -1
 	fmt.Println(oy)
 
+	windowSizeColumn, windowSizeRow = getWindowSize(s)
+
 	for {
 
-		s.ShowCursor(windowX, windowY)
+		editorRefreshScreen(s)
+		s.ShowCursor(currentColumn, currentRow)
 		editorDrawRows(s)
 		// Update screen
 		s.Show()
@@ -114,6 +159,7 @@ func main() {
 		switch ev := ev.(type) {
 		case *tcell.EventResize:
 			s.Sync()
+			windowSizeColumn, windowSizeRow = getWindowSize(s)
 		case *tcell.EventKey:
 			editorProcessKeyPress(s, ev)
 		case *tcell.EventMouse:
